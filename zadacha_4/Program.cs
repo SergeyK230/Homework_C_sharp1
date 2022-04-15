c8ccad10f955e047b00a39e7d03229291439d147
sergey.k <k.sergey2010@yandex.ru>
2022-04-16 00:37:15 +0700

    add zadacha4

---
+++ zadacha_4/Program.cs
@@ -0,0 +1,14 @@
+﻿int a, b, c;
+a = 444;
+b = 5;
+c = 78;
+if (a > b)
+{
+    if (a > c) Console.WriteLine(a);
+    else Console.WriteLine(c);
+}
+else
+{
+    if (b > c) Console.WriteLine(b);
+    else Console.WriteLine(c);
+}
